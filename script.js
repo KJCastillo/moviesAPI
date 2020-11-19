1fbc709297379829e091f4479e5acef9b78bfad9
Kevin Castillo <kevincastillo318@gmail.com>
2020-11-19 15:42:08 -0500

    css styling

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ function fetchData(){
         const html = data.Search.map(film => {
             return `
             <div class="films">
-                <p><img src="${film.Poster}" alt="movie poster" /></p>
-                <p>Title = ${film.Title}</p>
-                <p>Year = ${film.Year}</p>
+                <p class="row"><img src="${film.Poster}" alt="movie poster" /></p>
+                <p class="row">Title = ${film.Title}</p>
+                <p class="row">Year = ${film.Year}</p>
             </div>
             `
         }).join("")
