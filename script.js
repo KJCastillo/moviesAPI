7c995535739918828dfaa051ad8472be44457de7
Kevin Castillo <kevincastillo318@gmail.com>
2020-11-20 17:43:27 -0500

    working on input searchText

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
-function fetchData(){
-    fetch("http://www.omdbapi.com/?s=batman&apikey=ce941a34")
-    .then(response => {
-        if(!response.ok){
-            throw Error("error");
-        }
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.Search)
-        const html = data.Search.map(film => {
-            return `
-            <div class="films">
-                <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
-                <li class="titleLine">${film.Title} - released in ${film.Year}</li>
-            </div>
-            `
-        }).join("")
-        console.log(html)
-        document
-        .querySelector("#movies")
-        .insertAdjacentHTML("afterbegin", html)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-fetchData()
-
-// $(document).ready(() => {
-//     $('#searchForm').on('submit', (e) => {
-//       let searchText = document.querySelector('#searchText').value;
-//       getMovies(searchText);
-//       e.preventDefault();
-//     });
-// });
-
-// function getMovies(searchText){
-//     axios.get('http://www.omdbapi.com?s='+searchText+"&apikey=ce941a34")
-//       .then((response) => {
-//         let movies = response.data.Search;
-//         let output = '';
-//         $.each(movies, (index, film) => {
-//           output += `
+// function fetchData(){
+//     fetch("http://www.omdbapi.com/?s=batman&apikey=ce941a34")
+//     .then(response => {
+//         if(!response.ok){
+//             throw Error("error");
+//         }
+//         return response.json()
+//     })
+//     .then(data => {
+//         console.log(data.Search)
+//         const html = data.Search.map(film => {
+//             return `
 //             <div class="films">
 //                 <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
 //                 <li class="titleLine">${film.Title} - released in ${film.Year}</li>
 //             </div>
-//           `;
-//         });  
-//         $('#movies').html(output);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-// };
+//             `
+//         }).join("")
+//         console.log(html)
+//         document
+//         .querySelector("#movies")
+//         .insertAdjacentHTML("afterbegin", html)
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+// }
+
+// fetchData()
+
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+      let searchText = document.querySelector('#searchText').value;
+      getMovies(searchText);
+      e.preventDefault();
+    });
+});
+
+
+// let test = document.querySelector('#searchText1').value;
+// console.log(test)
+// console.log('do i work?')
+
+function getMovies(searchText){
+    axios.get('http://www.omdbapi.com?s='+searchText+"&apikey=ce941a34")
+      .then((response) => {
+        let movies = response.data.Search;
+        let output = '';
+        $.each(movies, (index, film) => {
+          output += `
+            <div class="films">
+                <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
+                <li class="titleLine">${film.Title} - released in ${film.Year}</li>
+            </div>
+          `;
+        });  
+        $('#movies').html(output);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+};
