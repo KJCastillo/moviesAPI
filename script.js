2dff23aa1a51e892cecd7f9f11027a7602614879
Kevin Castillo <kevincastillo318@gmail.com>
2020-11-20 17:21:12 -0500

    updating API call

--- script.js
+++ script.js
@@ -26,5 +26,32 @@ function fetchData(){
     })
 }
 
-
 fetchData()
+
+// $(document).ready(() => {
+//     $('#searchForm').on('submit', (e) => {
+//       let searchText = document.querySelector('#searchText').value;
+//       getMovies(searchText);
+//       e.preventDefault();
+//     });
+// });
+
+// function getMovies(searchText){
+//     axios.get('http://www.omdbapi.com?s='+searchText+"&apikey=ce941a34")
+//       .then((response) => {
+//         let movies = response.data.Search;
+//         let output = '';
+//         $.each(movies, (index, film) => {
+//           output += `
+//             <div class="films">
+//                 <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
+//                 <li class="titleLine">${film.Title} - released in ${film.Year}</li>
+//             </div>
+//           `;
+//         });  
+//         $('#movies').html(output);
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+// };
