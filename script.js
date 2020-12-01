682e319fe88c82224cf78dddf3439ab1d55a8ad6
Kevin Castillo <kevincastillo318@gmail.com>
2020-12-01 17:37:30 -0500

    corrected js folder

--- script.js
+++ script.js
@@ -1,65 +1,6 @@
-// function fetchData(){
-//     fetch("http://www.omdbapi.com/?s=batman&apikey=ce941a34")
-//     .then(response => {
-//         if(!response.ok){
-//             throw Error("error");
-//         }
-//         return response.json()
-//     })
-//     .then(data => {
-//         console.log(data.Search)
-//         const html = data.Search.map(film => {
-//             return `
-//             <div class="films">
-//                 <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
-//                 <li class="titleLine">${film.Title} - released in ${film.Year}</li>
-//             </div>
-//             `
-//         }).join("")
-//         console.log(html)
-//         document
-//         .querySelector("#movies")
-//         .insertAdjacentHTML("afterbegin", html)
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })
-// }
-
-// fetchData()
-
-// $(document).ready(() => {
-//     $('#searchForm').on('submit', (e) => {
-//       let searchText = document.querySelector('#searchText').value;
-//       getMovies(searchText);
-//       e.preventDefault();
-//     });
-// });
-
-
-// function getMovies(searchText){
-//     axios.get('http://www.omdbapi.com?s='+searchText+"&apikey=ce941a34")
-//       .then((response) => {
-//         let movies = response.data.Search;
-//         let output = '';
-//         $.each(movies, (index, film) => {
-//           output += `
-//             <div class="films">
-//                 <li class="poster"><img src="${film.Poster}" alt="movie poster" /></li>
-//                 <li class="titleLine">${film.Title} - released in ${film.Year}</li>
-//             </div>
-//           `;
-//         });  
-//         $('#movies').html(output);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-// };
-
 function getMovies () {
   let textInput = document.getElementById('searchText').value
-  console.log("text", textInput)
+  console.log(textInput)
   axios.get('http://www.omdbapi.com?s=' + textInput + "&apikey=ce941a34")
     .then((response) => {
       let movies = response.data.Search;
@@ -78,3 +19,9 @@ function getMovies () {
       console.log(err);
     });
 };
+
+document.getElementById('searchText').addEventListener('keydown', event => {
+  if (event.key === 'Enter' || event.key === 13) {
+    return getMovies()
+  }
+});
